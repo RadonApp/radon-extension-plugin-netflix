4d29e19f39d4d05adfbf8cd643f34c098d70f802
Dean Gardiner <me@dgardiner.net>
2016-10-20 10:21:59 +1300

    Updated configuration service to use preference pages

--- src/services/configuration/options.js
+++ src/services/configuration/options.js
@@ -1,51 +1,54 @@
 import {
+    Group,
+    Page,
     CheckboxOption,
-    EnableOption,
-    Group
+    EnableOption
 } from 'eon.extension.framework/services/configuration/models';
 
 import Plugin from '../../core/plugin';
 
 
 export default [
-    new EnableOption(Plugin, 'enabled', 'Enabled', {
-        default: false,
+    new Page(Plugin, null, Plugin.title, [
+        new EnableOption(Plugin, 'enabled', 'Enabled', {
+            default: false,
 
-        contentScripts: Plugin.contentScripts,
-        permissions: Plugin.permissions
-    }),
-
-    new Group(Plugin, 'activity', 'Activity', [
-        new EnableOption(Plugin, 'activity.enabled', 'Enabled', {
-            default: true,
-            requires: ['enabled']
+            contentScripts: Plugin.contentScripts,
+            permissions: Plugin.permissions
         }),
 
-        new CheckboxOption(Plugin, 'activity.movies', 'Movies', {
-            default: true,
-            requires: ['activity.enabled']
-        }),
+        new Group(Plugin, 'activity', 'Activity', [
+            new EnableOption(Plugin, 'activity.enabled', 'Enabled', {
+                default: true,
+                requires: ['enabled']
+            }),
 
-        new CheckboxOption(Plugin, 'activity.episodes', 'Episodes', {
-            default: true,
-            requires: ['activity.enabled']
-        })
-    ]),
+            new CheckboxOption(Plugin, 'activity.movies', 'Movies', {
+                default: true,
+                requires: ['activity.enabled']
+            }),
 
-    new Group(Plugin, 'sync', 'Sync', [
-        new EnableOption(Plugin, 'sync.enabled', 'Enabled', {
-            default: true,
-            requires: ['enabled']
-        }),
+            new CheckboxOption(Plugin, 'activity.episodes', 'Episodes', {
+                default: true,
+                requires: ['activity.enabled']
+            })
+        ]),
 
-        new CheckboxOption(Plugin, 'sync.history', 'Watched history', {
-            default: true,
-            requires: ['sync.enabled']
-        }),
+        new Group(Plugin, 'sync', 'Sync', [
+            new EnableOption(Plugin, 'sync.enabled', 'Enabled', {
+                default: true,
+                requires: ['enabled']
+            }),
 
-        new CheckboxOption(Plugin, 'sync.ratings', 'Ratings', {
-            default: true,
-            requires: ['sync.enabled']
-        })
+            new CheckboxOption(Plugin, 'sync.history', 'Watched history', {
+                default: true,
+                requires: ['sync.enabled']
+            }),
+
+            new CheckboxOption(Plugin, 'sync.ratings', 'Ratings', {
+                default: true,
+                requires: ['sync.enabled']
+            })
+        ])
     ])
 ];
